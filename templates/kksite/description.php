54e4cde73f9e09f08eb36021f22d0e4e6bb409d8
Moses Kirillov <km.115@yandex.ru>
2019-09-30 05:56:04 +0300

    first commit

---
+++ templates/kksite/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'Новый шаблон для сайта КК',
+	'DESCRIPTION' => 'Это самый лучший шаблон для сайта КК'
+);
+?>
